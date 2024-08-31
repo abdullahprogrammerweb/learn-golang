44973206437c1496e2a713bf5833cd0d47fe12d1
Abdullah <114979134+abdullahprogrammerweb@users.noreply.github.com>
2024-08-31 08:11:07 +0000

    print

---
+++ print/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "Abdullah"
+	height := 100.29
+
+	fmt.Println("age: ", age, " height: ", height, "name	", name)
+
+	fmt.Printf("age is %d\n", age)
+	fmt.Printf("type of name %T\n", name)
+	fmt.Printf("type of height %T\n", height)
+
+}
